eb038892275dc82f2d1f8757a362ee37afc0e5af
Atharv9415-bit <atharv.singh777123@gmail.com>
2025-05-17 14:01:55 +0530

    change app.js

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ const Blog=require("./models/blogModel");
 const {checkForAuthenticationCookie}=require("./middleWares/middleWareForAuthentication");
 const {connectMongoDB}=require('./connectMongoDB');
 const cookieParser = require("cookie-parser");
-connectMongoDB(process.env.MONGO_URL).then(()=>console.log("mongoDB connected")).catch((error)=>console.log("mongoDB not connected due to some error"));
+connectMongoDB(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit the app if DB fails
+  });
+
 app.set("view engine","ejs");
 app.set("views",path.resolve("./views"));
 app.use(express.urlencoded({extended:false}));
